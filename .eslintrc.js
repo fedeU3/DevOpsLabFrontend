631e2907ecaa78d9dea3aaa190bd16df8c7b5563
Federico Huespe <federicohue@hotmail.com>
2026-01-28 16:27:49 -0300

    Add ESLint and Prettier configuration files; update package.json scripts

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+};
